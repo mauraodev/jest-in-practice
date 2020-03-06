62c0a89083680ce1d89f4a75db208fc7201efc2e
Mauro Nunes <mauraoemau@hotmail.com>
2020-03-06 15:10:11 -0300

    Test

---
+++ src/map/map.js
@@ -0,0 +1,12 @@
+'use strict'
+
+const map = (arr, func) => {
+    let newArr = []
+    for (let i = 0; i < arr.length; i++ ) {
+        newArr.push(func(arr[i], i))
+    }
+
+    return newArr
+}
+
+export default map
